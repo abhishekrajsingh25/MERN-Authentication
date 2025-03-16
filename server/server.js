c000ef934158de349d9c7196223568b01f3d5f1f
Abhishek Raj Singh <abhishekrajsingh2509@gmail.com>
2025-03-16 15:04:47 +0530

    MERN Authentication

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+
+const allowedOrigins = ["http://localhost:5173"];
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: allowedOrigins, credentials: true }));
+
+//api endpoints
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
